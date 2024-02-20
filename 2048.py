94ddbf6974cf7e931622ce7aca22edb3b992ecaa
Benjamin Wende <benni.wende@gmail.com>
2024-02-20 21:17:14 +0100

    Update 2048.py

--- 2048.py
+++ 2048.py
@@ -3,7 +3,8 @@ import time
 import customtkinter
 
 root = customtkinter.CTk()
-root.iconbitmap("Python-2048/icon.ico")
+ico_path = "icon.ico"
+root.iconbitmap(ico_path) # mit vorsicht zu genießen
 labels = [] #spielfeld (als Label)
 keylog = False #verhindert merge bei mehrfacheingabe
 
